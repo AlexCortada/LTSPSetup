8b33580b8e7df630a224fd128852672981d24237
Alex Cortada <alexander.cortada@gmail.com>
2024-12-13 23:55:10 -0500

    Update install.sh
    added cups and hp app gui / drivers

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ add-apt-repository ppa:ltsp
 
 apt update && apt upgrade --yes
 
-apt install --install-recommends ltsp ipxe dnsmasq nfs-kernel-server openssh-server squashfs-tools ethtool net-tools --y
+apt install --install-recommends ltsp ipxe dnsmasq nfs-kernel-server openssh-server squashfs-tools ethtool net-tools --yes
 
 ltsp dnsmasq
 
@@ -13,3 +13,9 @@ ltsp ipxe
 ltsp nfs
 
 ltsp initrd
+
+apt install cups
+
+apt-cache search hplip
+
+sudo apt install hplip hplip-gui
